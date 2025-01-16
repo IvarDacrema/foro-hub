34db52f1bb6edab573b149b6af3c8244579a32f5
Ivar Dacrema <ivar.m.dacrema@gmail.com>
2025-01-16 14:32:56 -0300

    1 upluoad proyect

---
+++ database/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO users(user_name, email, user_password)
+VALUES('user', 'user@email.com', '$2a$12$y1C1sKJguGuD2k63Ek1IE.pW143W.4hsPtX2ZYW2NpjsNtMRubMg6')
+
+INSERT INTO courses(name, category)
+VALUES('Java', 'PROGRAMMING')
+
+INSERT INTO courses(name, category)
+VALUES('Python', 'DATA_SCIENCE')
+
+INSERT INTO courses(name, category)
+VALUES('Linux', 'DEV_OPS')
+
+INSERT INTO courses(name, category)
+VALUES('Autocad', 'GRAPHIC_DESIGN')
+
+INSERT INTO courses(name, category)
+VALUES('Images', 'MARKETING')
+
+INSERT INTO courses(name, category)
+VALUES('To Be', 'ENGLISH')
